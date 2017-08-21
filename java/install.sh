7a072a19b6a7e5bd8bc0b6992a3e084f0a72ae6d
Bharath Upadhya <1992bharath@gmail.com>
2017-08-21 19:40:31 +0530

    y

--- java/install.sh
+++ java/install.sh
@@ -8,12 +8,12 @@ echo "================= Installing openjdk-8-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
 apt-get install -y openjdk-8-jdk=8u131-b11-2ubuntu1.16.04.3
-update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-arm64/jre/bin/java
+update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-arm64/bin/javac
 add-apt-repository ppa:maarten-fonville/ppa
 apt-get update
 apt-get install icedtea-8-plugin=1.6.2-3ubuntu1
-update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/javaws
+update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-arm64/jre/bin/javaws
 
 echo "================ Installing oracle-java8-installer ================="
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
